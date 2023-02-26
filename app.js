f867ce0a77411f3a3953d2cc4a020b7b1c7ae8e0
Lucas Miranda <lucasmiranda@Lucass-MacBook-Pro.local>
2023-02-26 10:32:37 -0300

    fix(port): Fixing default port to 8080 due to Container uses

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = process.env.PORT || 3200;
+const port = process.env.PORT || 8080;
 
 function allowOrigin(req, res, next) {
   // Website you wish to allow to connect
